23b2a94c75f5dfca465fe389a11c7f35f54fc5a9
Ольга Омельчук <olgaomelchuk@bk.ru>
2025-09-24 20:46:31 +0500

    Задание 2

---
+++ task02/task02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первую сторону прямоугольника");
+            var a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите вторую сторону прямоугольника");
+            var b = double.Parse(Console.ReadLine());
+
+            var area = a * b;
+            var p = (a + b) * 2;
+
+            var double_a = Math.Pow(a, 2);
+            var double_b = Math.Pow(b, 2);
+            var d = Math.Sqrt(double_a+double_b);
+
+            Console.WriteLine("Площадь прямоугольника равна " + area);
+            Console.WriteLine("Периметр прямоугольника равен " + p);
+            Console.WriteLine("Длина диагонали прямоугольника равна " + d);
+        }
+    }
+}
